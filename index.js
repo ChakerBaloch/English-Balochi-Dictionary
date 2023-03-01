69f8a8c1c0ed40d855f22e39430d0266fe259d76
chaker <chakerbaloch90@gmail.com>
2023-02-28 19:30:33 -0800

    make menu dropdown undelined, change color, resize according to media query, remove exit button

---
+++ index.js
@@ -0,0 +1,47 @@
+{
+    "back-heel": {
+      "pronunciation": "/ˈbakˌhiːl/",
+      "type": "v.",
+      "meaning": "Pāɔ ay pinz ā gon lagať janag"
+    },
+    "backing": {
+      "pronunciation": "/ˈbakɪŋ/",
+      "type": "n.1.",
+      "meaning": "Māli o exlāki komak. 2. Sahārā"
+    },
+    "backland": {
+      "pronunciation": "/ˈbakˌlænd/",
+      "type": "n.",
+      "meaning": "Kuccag"
+    },
+    "backlash": {
+      "pronunciation": "/ˈbaklaʃ/",
+      "type": "n.",
+      "meaning": "Nawaśśi. Nārāzi zaher kanag ay amal. Pabbi. Saxťein Passau. Saxťein raɔ ay amal"
+    },
+    "backlog": {
+      "pronunciation": "/ˈbaklɒɡ/",
+      "type": "n.",
+      "meaning": "Manťagein yā paśť kapťagein kanagi kār"
+    },
+    "backpack": {
+      "pronunciation": "/ˈbakpak/",
+      "type": "n.",
+      "meaning": "Baddi. Pośť ay banɔoakein bandóri"
+    },
+    "backpacker": {
+      "pronunciation": "/ˈbakpakə/",
+      "type": "n.",
+      "meaning": "Pāɔgoal. Pośť ā sāmān ladeťagein marɔom"
+    },
+    "backpacking": {
+      "pronunciation": "/ˈbækˌpæk.ɪŋ/",
+      "type": "n.",
+      "meaning": "Pāɔgóli"
+    },
+    "backrest": {
+      "pronunciation": "/ˈbakrɛst/",
+      "type": "n.",
+      "meaning": "Pośťārām. Pośť ay sahārā ɔiokein ciz"
+    }
+  }  
